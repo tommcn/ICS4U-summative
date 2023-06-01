5c7eb3938f148d60c7f4adedd298295620ef6cf5
Tomas McNamer <tommcn@mcnamer.ca>
2023-06-01 19:07:18 +0000

    write variables

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+from numpy import var
 from eval_postfix import eval_postfix
 from infix_to_postfix import infix_to_postfix
 from operators import Operators
 from adt import Stack
+from string import ascii_letters
+
+ACCEPTABLE_VARIABLE_CHARACTERS = ascii_letters + "_"
 
 IFNAME = "McNamer_Tomas_input.txt"
 OFNAME = "McNamer_Tomas_output.txt"
 
-def create_postfix_equation():
-    s = Stack.from_list([2, 3, 1, Operators.MUL, Operators.ADD, 9, Operators.SUB])
-    return s
+
+def pipeline(expr: str, variables):
+    varname = None
+    if expr[0] in ACCEPTABLE_VARIABLE_CHARACTERS:
+        varname, expr = expr.split("=")
+    postfix = infix_to_postfix(expr)
+    result = eval_postfix(postfix)
+    if varname is not None:
+        variables[varname] = result
+
+    return result, variables
+
 
 def main():
     results = []
+    variables = {
+        "PI": 3.14159,
+        "e": 2.71828
+    }
     with open(IFNAME, "r", encoding="UTF-8") as f:
         for line in f.readlines():
-            postfix = infix_to_postfix(line)
-            result = eval_postfix(postfix)
+            result, variables = pipeline(line, variables)
             results.append(str(result) + "\n")
+            print(variables)
 
     with open(OFNAME, "w", encoding="UTF-8") as f:
         f.writelines(results)
